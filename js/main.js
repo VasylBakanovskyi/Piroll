20d25e366f1c87c6493ccf29ae4e5db3c0a42b2d
Vasyl Bakanovskyi <bakanovskyivasyl@gmail.com>
2019-07-24 21:20:04 +0300

    add responsive

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuSide = document.querySelector('.menu-side');
+const menuIcon = document.querySelector('.menu-mobile img');
+    
+document.body.addEventListener('click', function(e) {
+    const el = e.target;
+    
+    if (el === menuIcon) {
+        menuSide.classList.toggle('hidden');
+        return;
+    }
+    if (el !== menuSide && !el.closest('.menu-side')) {
+        menuSide.classList.add('hidden');
+    }
+
+})
