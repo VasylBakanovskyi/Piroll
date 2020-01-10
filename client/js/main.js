4358dfeb274b9c772307456027cd20f881e0126b
VasylBakanovskyiOSF <vasyl.bakanovskyi@osf-commerce.com>
2020-01-10 23:11:25 +0200

    add colors variables and start refactor css

--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,15 @@
-const menuSide = document.querySelector('.menu-side');
-const menuIcon = document.querySelector('.menu-mobile img');
+const menuSide = document.querySelector('.js-menu-aside');
+const menuIcon = document.querySelector('.js-burger-menu');
 
-document.body.addEventListener('click', function(e) {
+document.body.addEventListener('click', (e) => {
     const el = e.target;
-    
-    if (el === menuIcon) {
+
+    if (el === menuIcon || el.closest('.js-burger-menu')) {
         menuSide.classList.toggle('is-hidden');
         return;
     }
-    if (el !== menuSide && !el.closest('.menu-side')) {
+
+    if (el !== menuSide && !el.closest('.js-menu-aside')) {
         menuSide.classList.add('is-hidden');
     }
 })
